40f3332c708a562809f81059064eec0d65d1c151
Emery Mitchem <emery_mitchem@csd28j.org>
2026-06-08 10:49:28 -0700

    Add specialization.js placeholder

---
+++ src/js/specialization.js
@@ -0,0 +1,15 @@
+// specialization.js - Placeholder for Specialization System
+// This file will be populated with the complete specialization system
+
+class SpecializationSystem {
+    constructor() {
+        this.specializations = [];
+        this.active = null;
+    }
+
+    init() {
+        console.log("SpecializationSystem initialized");
+    }
+}
+
+window.SpecializationSystem = SpecializationSystem;

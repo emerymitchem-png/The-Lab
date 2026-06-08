8749e0dc7451a872d8d4947e59d4e138d51d7a33
Emery Mitchem <emery_mitchem@csd28j.org>
2026-06-08 10:55:05 -0700

    Add shop.js placeholder

---
+++ src/js/shop.js
@@ -0,0 +1,15 @@
+// shop.js - Placeholder for Shop System
+// This file will be populated with the complete shop system
+
+class ShopSystem {
+    constructor() {
+        this.inventory = [];
+        this.active = false;
+    }
+
+    init() {
+        console.log("ShopSystem initialized");
+    }
+}
+
+window.ShopSystem = ShopSystem;
